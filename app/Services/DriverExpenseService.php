a9faa6f9de4daee23b669c7eba415299d3bcf08d
enzofxx <35643620+enzofxx@users.noreply.github.com>
2022-12-22 09:52:14 +0200

    Task commit

--- app/Services/DriverExpenseService.php
+++ app/Services/DriverExpenseService.php
@@ -8,9 +8,35 @@ class DriverExpenseService
     {
         $result = [];
 
-        // Write your logic here.
-        // ...
+        $result[] = ['Expense/Driver', 'Amount, $', $drivers[0], $drivers[1]];
 
+        $oddCount = 0;
+        foreach($expenses as $key => $expense){
+            $expense = intval(floor(floatval($expense)*100+0.5));
+            if($expense % 2 == 0){
+                $expense = $expense / 100;
+                $result[] = [$key, number_format($expense,2), number_format($expense/2,2), number_format($expense/2,2)];
+            } else if ($expense % 2 == 1){
+                if($oddCount % 2 === 0){
+                    $expense = $expense / 100;
+                    $result[] = [$key, number_format($expense,2), number_format(($expense/2)+0.005,2), number_format(($expense/2)-0.005,2)];
+                    ++$oddCount;
+                } else {
+                    $expense = $expense / 100;
+                    $result[] = [$key, number_format($expense,2), number_format(($expense/2)-0.005,2), number_format(($expense/2)+0.005,2)];
+                    ++$oddCount;
+                }
+            }
+        }
+
+        $driver1Total = 0;
+        $driver2Total = 0;
+        foreach($result as $value) {
+            $driver1Total += floatval($value[2]);
+            $driver2Total += floatval($value[3]);
+        }
+        
+        $result[] = ['Total:', number_format($driver1Total+$driver2Total,2), number_format($driver1Total,2), number_format($driver2Total,2)];
         return $result;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,28 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Test Task</title>
+        <style>
+            table {
+                border: solid black 1px;
+            }
+            td {
+                padding-right: 15px;
+            }
+        </style>
     </head>
     <body>
-        <div>
+        <!-- <div>
             <pre>{{ print_r($data, true) }}</pre>
-        </div>
+        </div> -->
+        <table>
+            @foreach($data as $result)
+                <tr>
+                    <td>{{ $result[0] }}</td>
+                    <td>{{ $result[1] }}</td>
+                    <td>{{ $result[2] }}</td>
+                    <td>{{ $result[3] }}</td>
+                </tr>
+            @endforeach
+        </table>
     </body>
 </html>
